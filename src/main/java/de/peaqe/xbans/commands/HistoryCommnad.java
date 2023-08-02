f3a7cbff19ba41061ba4d27903df0d8161a300a1
BuildTools <unconfigured@null.spigotmc.org>
2023-08-02 20:50:56 +0200

    Add file

---
+++ src/main/java/de/peaqe/xbans/commands/HistoryCommnad.java
@@ -0,0 +1,76 @@
+package de.peaqe.xbans.commands;
+
+import de.peaqe.xbans.XBans;
+import de.peaqe.xbans.provider.BanHistoryDatabase;
+import de.peaqe.xbans.utils.IDUtils;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
+
+import java.util.ArrayList;
+
+
+/*
+ *
+ *  Class by peaqe created in 2023
+ *  Class: HistoryCommnad
+ *
+ *  Information's:
+ *  Type: Java-Class | Bungeecord Command
+ *  Created: 02.08.2023 / 20:33
+ *
+ */
+
+@SuppressWarnings(value = "unused")
+public class HistoryCommnad extends Command implements TabExecutor {
+
+    public HistoryCommnad(String name) {
+        super(name);
+    }
+
+	XBans main = XBans.instance;
+	BanHistoryDatabase historyDatabase = main.getBanHistoryDatabase();
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+		if (!(sender.hasPermission("system.ban.history"))) {
+			sender.sendMessage(TextComponent.fromLegacyText(main.prefix + "Du hast nicht die benötigte Berechtigung, um diesen Command ausführen zu können!"));
+			return;
+		}
+
+		if (!(args.length == 1)) {
+			sender.sendMessage(TextComponent.fromLegacyText(main.prefix + "Bitte verwende: /history (Spieler)"));
+			return;
+		}
+
+		if (main.getPlayerDatabase().isRegistered(args[0])) {
+			sender.sendMessage(TextComponent.fromLegacyText(main.prefix + "Der Spieler " + main.getColor() + "§7 konnte nicht gefunden werden!"));
+			return;
+		}
+
+		var list = historyDatabase.getPlayerBans(args[0]);
+
+		sender.sendMessage(TextComponent.fromLegacyText(main.prefix + "Die Vergangenheit, von " + main.getColor() + args[0] + "§7:"));
+		sender.sendMessage(TextComponent.fromLegacyText(""));
+
+		sender.sendMessage(TextComponent.fromLegacyText(main.prefix + "Gebannt: " + (main.getBanDatabase().isBanned(args[0]) ? "§aJa" : "§cNein")));
+		sender.sendMessage(TextComponent.fromLegacyText(""));
+
+		sender.sendMessage(TextComponent.fromLegacyText(main.prefix + "Bans: "));
+		list.forEach(document -> {
+			sender.sendMessage(TextComponent.fromLegacyText("§8» Grund: " + main.getColor() + document.getString("ban_reason") + "§7 Laufzeit: " + main.getColor() + main.idManager.getExpiry(main.idManager.getBanID(document.getInteger("ban_id"))) +
+					" §7(" + main.getColor() + new IDUtils(main.idManager.getBanID(document.getInteger("ban_id"))).getBanExpiryDate() + "§7)"));
+		});
+
+
+        return;
+    }
+
+
+    @Override
+    public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+        return new ArrayList<>();
+    }
+}
---
+++ src/main/java/de/peaqe/xbans/provider/BanHistoryDatabase.java
@@ -0,0 +1,94 @@
+package de.peaqe.xbans.provider;
+/*
+ *
+ *  Class by peaqe created in 2023
+ *  Class: BanDatabase
+ *
+ *  Information's:
+ *  Type: Java-Class
+ *  Created: 02.08.2023 / 11:39
+ *
+ */
+
+import com.google.gson.Gson;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import de.peaqe.xbans.XBans;
+import de.peaqe.xbans.utils.BanID;
+import de.peaqe.xbans.utils.IDUtils;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.scheduler.TaskScheduler;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings(value = "unused")
+public class BanHistoryDatabase {
+
+    MongoClient mongoClient;
+    private final MongoCollection<Document> collection;
+    private final Gson gson = new Gson();
+
+    @SuppressWarnings(value = "deprecation")
+    public BanHistoryDatabase() {
+
+        var mongo_config = XBans.instance.getDatabaseConfig();
+
+        MongoCredential mongoCredential = MongoCredential.createCredential("noviaplugins", "noviamc", "noviapluginpassword".toCharArray());
+
+        mongoClient = new MongoClient(new ServerAddress("127.0.0.1", 27017), List.of(mongoCredential));
+        MongoDatabase database = mongoClient.getDatabase("noviamc");
+        String collectionName = "xbans_history";
+        collection = database.getCollection(collectionName);
+
+        MongoCollection<Document> mongoService = database.getCollection(collectionName);
+    }
+
+    public void banPlayer(String autor, String name, BanID banID) {
+        if (!XBans.instance.getPlayerDatabase().isRegistered(name)) {
+            return;
+        }
+
+        UUID uuid = XBans.instance.getPlayerDatabase().getPlayerUUID(name);
+        IDUtils idUtils = new IDUtils(banID);
+
+        Document document = new Document("player_uuid", uuid.toString())
+                .append("player_name", name)
+                .append("ban_autor", autor)
+                .append("ban_id", idUtils.getIdentifier())
+                .append("ban_reason", idUtils.getReason())
+                .append("ban_duration", idUtils.getBanDuration())
+                .append("ban_expiry", idUtils.getBanExpiry())
+                .append("ban_level", idUtils.getBanLevel())
+                .append("ban_type", idUtils.getBanType());
+
+        collection.insertOne(document);
+    }
+
+    public List<Document> getPlayerBans(String playerName) {
+        List<Document> playerBans = new ArrayList<>();
+
+        Bson filter = Filters.eq("player_name", playerName);
+        FindIterable<Document> documents = collection.find(filter);
+
+        for (Document document : documents) {
+            playerBans.add(document);
+        }
+
+        return playerBans;
+    }
+
+    public int getPlayerBanSize(String name) {
+        return this.getPlayerBans(name).size();
+    }
+
+}
